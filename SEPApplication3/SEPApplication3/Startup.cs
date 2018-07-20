68b594e834a1eae56a2c2df4de069f1e91bf5671
tandang94 <danganhtan94@gmail.com>
2018-07-20 16:15:59 +0700

    EZ nhu 1 tro dua

---
+++ SEPApplication3/SEPApplication3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SEPApplication3.Startup))]
+namespace SEPApplication3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
